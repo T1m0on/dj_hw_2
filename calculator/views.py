a20d2ca8317d7f51eb0cb7f92b072d5abff46ad9
Timofey Prokhorov <prokhorovtd@yandex.ru>
2023-05-22 13:10:29 +0300

    Initial commit

---
+++ calculator/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from django.urls import reverse
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+def get_recipe(dish, count):
+    result = {}
+    for k, v in DATA[dish].items():
+        result[k] = v * count
+    return result
+
+
+def recipe(request, dish):
+    template_name = 'calculator/index.html'
+    try:
+        count = int(request.GET.get('count'))
+    except:
+        count = 1
+    recipe = get_recipe(dish, count)
+    context = {'recipe': recipe, 'dish': dish}
+    return render(request, template_name, context)
+
+
+def home_view(request):
+    template_name = 'calculator/recipes.html'
+
+    pages = {
+        'Омлет': '/omlet',
+        'Паста': '/pasta',
+        'Бутер': '/buter',
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
